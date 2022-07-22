3efa6cd5e1bd7162725fe7d91bb179c08566f426
ESRA KURT <esra.kurt97833@gmail.com>
2022-07-22 15:34:48 +0300

    Add context database

---
+++ ProjeCore/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjeCore.Models
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=HUAWEI;database=ProjeCore; integrated security=true;");
+        }
+        public DbSet<Birim> Birims { get; set; }
+        public DbSet<Personel> Personels { get; set; }
+    }
+}
