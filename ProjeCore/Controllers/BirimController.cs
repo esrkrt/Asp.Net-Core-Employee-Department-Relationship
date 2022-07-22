fec2ae8113ff61351a2436d1fd32199792438ecf
ESRA KURT <esra.kurt97833@gmail.com>
2022-07-22 15:50:44 +0300

    Add unit screen listing

---
+++ ProjeCore/Controllers/BirimController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjeCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjeCore.Controllers
+{
+    public class BirimController : Controller
+    {
+        Context c = new Context();
+        public IActionResult Index()
+        {
+            var value = c.Birims.ToList();
+            return View(value);
+        }
+    }
+}
