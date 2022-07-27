f3bef0326a7669e0f64f61ae706849355fb63999
ESRA KURT <esra.kurt97833@gmail.com>
2022-07-27 14:05:18 +0300

    Add employee page add new employee

--- ProjeCore/Controllers/PersonelController.cs
+++ ProjeCore/Controllers/PersonelController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ProjeCore.Models;
 using System;
@@ -15,6 +16,28 @@ namespace ProjeCore.Controllers
         {
             var value = c.Personels.Include(x => x.Birim).ToList();
             return View(value);
+        }
+        [HttpGet]
+        public IActionResult AddEmployee()
+        {
+            List<SelectListItem> values = (from x in c.Birims.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.BirimAd,
+                                               Value = x.BirimID.ToString()
+                                           }).ToList();
+            ViewBag.dgr = values;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddEmployee(Personel b)
+        {
+            var per = c.Birims.Where(x => x.BirimID == b.Birim.BirimID).FirstOrDefault();
+            b.Birim = per;
+            c.Personels.Add(b);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+
         }
     }
 }
